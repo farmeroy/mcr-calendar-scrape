ced9de8ffd36e475925b870b1e7508945e8a1a9f
Raffaele Cataldo <kemanicataldo@gmail.com>
2023-11-29 11:31:23 -0800

    feat: try async wip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use chrono::{DateTime, Local, NaiveDate};
 use regex::Regex;
-use reqwest::{self, blocking::get};
+use reqwest::{self, blocking::get, Client};
 use scraper::{self, Html, Selector};
 
 #[derive(Debug)]
@@ -10,7 +10,8 @@ struct HouseDates {
     check_outs: Vec<NaiveDate>,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let houses_response = get("https://www.mendocinovacations.com/houses");
     let html = houses_response.unwrap().text().unwrap();
     let data = Html::parse_document(&html);
@@ -25,83 +26,96 @@ fn main() {
             }
         }
     }
+    let client = Client::new();
+
+    let mut tasks = vec![];
 
     for link in links {
-        let link = format!("{link}/calendar");
-        let response = get(link);
+        let client_clone = client.clone();
 
-        let data = response.unwrap().text().unwrap();
-        let date_regex = Regex::new(r"(?<date>\d{4}-\d{2}-\d{2})").unwrap();
+        let task = tokio::spawn(async move {
+            let link = format!("{link}/calendar");
+            let response = client_clone.get(link).send().await;
 
-        let house_name_selector = Selector::parse("h1 > a").unwrap();
+            let data = response.unwrap().text().await.unwrap();
+            let date_regex = Regex::new(r"(?<date>\d{4}-\d{2}-\d{2})").unwrap();
 
-        let calendar_selector = Selector::parse("div.calendar-container").unwrap();
-        let document = Html::parse_document(&data);
+            let house_name_selector = Selector::parse("h1 > a").unwrap();
 
-        let house_name = document
-            .select(&house_name_selector)
-            .next()
-            .unwrap()
-            .first_child()
-            .unwrap()
-            .value();
-        println!("{:?}", house_name);
-        let calendars = document.select(&calendar_selector);
-        let mut check_outs: Vec<NaiveDate> = Vec::new();
-        let mut check_ins: Vec<NaiveDate> = Vec::new();
+            let calendar_selector = Selector::parse("div.calendar-container").unwrap();
+            let document = Html::parse_document(&data);
 
-        for calendar in calendars {
-            let month = calendar
-                .first_child()
-                .unwrap()
-                .next_sibling()
+            let house_name = document
+                .select(&house_name_selector)
+                .next()
                 .unwrap()
                 .first_child()
                 .unwrap()
                 .value();
-            let fragment = Html::parse_fragment(&calendar.html());
-            let checkout_selector = Selector::parse("div.calendar-checkout").unwrap();
-            let dates = fragment.select(&checkout_selector);
-            for date in dates {
-                let check_out_date_url = date
-                    .prev_sibling()
-                    .unwrap()
-                    .value()
-                    .as_element()
-                    .and_then(|a| a.attr("href"));
-                if let Some(check_out_date) =
-                    date_regex.find(check_out_date_url.unwrap_or_default())
-                {
-                    check_outs.push(
-                        NaiveDate::parse_from_str(check_out_date.as_str(), "%Y-%m-%d").unwrap(),
-                    );
-                } else {
-                }
-            }
-            let fragment = Html::parse_fragment(&calendar.html());
-            let checkout_selector = Selector::parse("div.calendar-checkin").unwrap();
-            let dates = fragment.select(&checkout_selector);
-            for date in dates {
-                if let Some(check_out_div) = date.next_sibling() {
-                    if let Some(check_out_date) = check_out_div.first_child() {
-                        let mut day = check_out_date.value().as_text().unwrap().text.to_string();
-                        if day.len() < 2 {
-                            day = format!("0{day}")
-                        };
+            println!("{:?}", house_name);
+            let calendars = document.select(&calendar_selector);
+            let mut check_outs: Vec<NaiveDate> = Vec::new();
+            let mut check_ins: Vec<NaiveDate> = Vec::new();
 
-                        let date_string = format!("{} {}", day, month.as_text().unwrap().text);
-                        check_ins.push(
-                            NaiveDate::parse_from_str(&date_string, "%d %B %Y").unwrap_or_default(),
-                        )
+            for calendar in calendars {
+                let month = calendar
+                    .first_child()
+                    .unwrap()
+                    .next_sibling()
+                    .unwrap()
+                    .first_child()
+                    .unwrap()
+                    .value();
+                let fragment = Html::parse_fragment(&calendar.html());
+                let checkout_selector = Selector::parse("div.calendar-checkout").unwrap();
+                let dates = fragment.select(&checkout_selector);
+                for date in dates {
+                    let check_out_date_url = date
+                        .prev_sibling()
+                        .unwrap()
+                        .value()
+                        .as_element()
+                        .and_then(|a| a.attr("href"));
+                    if let Some(check_out_date) =
+                        date_regex.find(check_out_date_url.unwrap_or_default())
+                    {
+                        check_outs.push(
+                            NaiveDate::parse_from_str(check_out_date.as_str(), "%Y-%m-%d").unwrap(),
+                        );
+                    } else {
+                    }
+                }
+                let fragment = Html::parse_fragment(&calendar.html());
+                let checkout_selector = Selector::parse("div.calendar-checkin").unwrap();
+                let dates = fragment.select(&checkout_selector);
+                for date in dates {
+                    if let Some(check_out_div) = date.next_sibling() {
+                        if let Some(check_out_date) = check_out_div.first_child() {
+                            let mut day =
+                                check_out_date.value().as_text().unwrap().text.to_string();
+                            if day.len() < 2 {
+                                day = format!("0{day}")
+                            };
+
+                            let date_string = format!("{} {}", day, month.as_text().unwrap().text);
+                            check_ins.push(
+                                NaiveDate::parse_from_str(&date_string, "%d %B %Y")
+                                    .unwrap_or_default(),
+                            )
+                        }
                     }
                 }
             }
-        }
-        let house = HouseDates {
-            house_name: house_name.as_text().unwrap().text.to_string(),
-            check_ins: check_ins.clone(),
-            check_outs: check_outs.clone(),
-        };
-        println!("{:?}", house)
+            let house = HouseDates {
+                house_name: house_name.as_text().unwrap().text.to_string(),
+                check_ins: check_ins.clone(),
+                check_outs: check_outs.clone(),
+            };
+            println!("{:?}", house);
+        });
+        tasks.push(task);
+    }
+    for task in tasks {
+        task.await.expect("Failed to await task");
     }
 }
