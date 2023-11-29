bc0b7055d5dc5aa5625b14fa247525fefc88e38a
Raffaele Cataldo <kemanicataldo@gmail.com>
2023-11-29 10:46:44 -0800

    feat: scrape website and return list of houses with check in and check out dates

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,107 @@
-use chrono::{DateTime, Local};
+use chrono::{DateTime, Local, NaiveDate};
+use regex::Regex;
 use reqwest::{self, blocking::get};
 use scraper::{self, Html, Selector};
 
-struct HouseCheckIn {
-    check_ins: Vec<DateTime<Local>>,
-    check_outs: Vec<DateTime<Local>>,
+#[derive(Debug)]
+struct HouseDates {
+    house_name: String,
+    check_ins: Vec<NaiveDate>,
+    check_outs: Vec<NaiveDate>,
 }
 
 fn main() {
-    let response =
-        get("https://www.mendocinovacations.com/houses/asst-lightkeepers-house/calendar");
+    let houses_response = get("https://www.mendocinovacations.com/houses");
+    let html = houses_response.unwrap().text().unwrap();
+    let data = Html::parse_document(&html);
+    let selector = Selector::parse("a").unwrap();
 
-    let data = response.unwrap().text().unwrap();
+    let mut links: Vec<String> = Vec::new();
 
-    let calendar_selector = Selector::parse("div.calendar-container").unwrap();
-    let document = Html::parse_document(&data);
-    let calendars = document.select(&calendar_selector);
+    for link in data.select(&selector) {
+        if let Some(href) = link.value().attr("href") {
+            if href.starts_with("https://www.mendocinovacations.com/houses/") {
+                links.push(href.to_string());
+            }
+        }
+    }
 
-    for calendar in calendars {
-        let month = calendar
-            .first_child()
-            .unwrap()
-            .next_sibling()
+    for link in links {
+        let link = format!("{link}/calendar");
+        let response = get(link);
+
+        let data = response.unwrap().text().unwrap();
+        let date_regex = Regex::new(r"(?<date>\d{4}-\d{2}-\d{2})").unwrap();
+
+        let house_name_selector = Selector::parse("h1 > a").unwrap();
+
+        let calendar_selector = Selector::parse("div.calendar-container").unwrap();
+        let document = Html::parse_document(&data);
+
+        let house_name = document
+            .select(&house_name_selector)
+            .next()
             .unwrap()
             .first_child()
             .unwrap()
             .value();
-        println!("{:?}", month);
-        let fragment = Html::parse_fragment(&calendar.html());
-        let checkout_selector = Selector::parse("div.calendar-checkout").unwrap();
-        let dates = fragment.select(&checkout_selector);
-        for date in dates {
-            let check_out_date = date.next_sibling().unwrap().first_child().unwrap().value();
-            println!("{:?}", check_out_date);
+        println!("{:?}", house_name);
+        let calendars = document.select(&calendar_selector);
+        let mut check_outs: Vec<NaiveDate> = Vec::new();
+        let mut check_ins: Vec<NaiveDate> = Vec::new();
+
+        for calendar in calendars {
+            let month = calendar
+                .first_child()
+                .unwrap()
+                .next_sibling()
+                .unwrap()
+                .first_child()
+                .unwrap()
+                .value();
+            let fragment = Html::parse_fragment(&calendar.html());
+            let checkout_selector = Selector::parse("div.calendar-checkout").unwrap();
+            let dates = fragment.select(&checkout_selector);
+            for date in dates {
+                let check_out_date_url = date
+                    .prev_sibling()
+                    .unwrap()
+                    .value()
+                    .as_element()
+                    .and_then(|a| a.attr("href"));
+                if let Some(check_out_date) =
+                    date_regex.find(check_out_date_url.unwrap_or_default())
+                {
+                    check_outs.push(
+                        NaiveDate::parse_from_str(check_out_date.as_str(), "%Y-%m-%d").unwrap(),
+                    );
+                } else {
+                }
+            }
+            let fragment = Html::parse_fragment(&calendar.html());
+            let checkout_selector = Selector::parse("div.calendar-checkin").unwrap();
+            let dates = fragment.select(&checkout_selector);
+            for date in dates {
+                if let Some(check_out_div) = date.next_sibling() {
+                    if let Some(check_out_date) = check_out_div.first_child() {
+                        let mut day = check_out_date.value().as_text().unwrap().text.to_string();
+                        if day.len() < 2 {
+                            day = format!("0{day}")
+                        };
+
+                        let date_string = format!("{} {}", day, month.as_text().unwrap().text);
+                        check_ins.push(
+                            NaiveDate::parse_from_str(&date_string, "%d %B %Y").unwrap_or_default(),
+                        )
+                    }
+                }
+            }
         }
+        let house = HouseDates {
+            house_name: house_name.as_text().unwrap().text.to_string(),
+            check_ins: check_ins.clone(),
+            check_outs: check_outs.clone(),
+        };
+        println!("{:?}", house)
     }
 }
