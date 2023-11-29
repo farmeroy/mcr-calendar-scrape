ad3b225ba46016ed75581031fef2b5f081b7b836
Raffaele Cataldo <kemanicataldo@gmail.com>
2023-11-29 12:26:12 -0800

    use futures

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use std::time::Duration;
-
 use chrono::NaiveDate;
-use futures::future::try_join_all;
+use futures::future;
 use regex::Regex;
-use reqwest::{self, get};
+use reqwest::{self, get, Client};
 use scraper::{self, Html, Selector};
 
 #[derive(Debug)]
@@ -30,85 +28,90 @@ async fn main() {
         }
     }
 
-    let tasks = links.into_iter().map(|link| async move {
-        let link = format!("{link}/calendar");
-        let response = get(link);
-        let data = response.await.unwrap().text().await.unwrap();
-        let date_regex = Regex::new(r"(?<date>\d{4}-\d{2}-\d{2})").unwrap();
+    let client = Client::new();
+    let tasks = future::join_all(links.into_iter().map(|link| {
+        let client = &client;
+        async move {
+            let link = format!("{link}/calendar");
+            let response = client.get(link).send();
+            let data = response.await.unwrap().text().await.unwrap();
+            let date_regex = Regex::new(r"(?<date>\d{4}-\d{2}-\d{2})").unwrap();
 
-        let house_name_selector = Selector::parse("h1 > a").unwrap();
+            let house_name_selector = Selector::parse("h1 > a").unwrap();
 
-        let calendar_selector = Selector::parse("div.calendar-container").unwrap();
-        let document = Html::parse_document(&data);
+            let calendar_selector = Selector::parse("div.calendar-container").unwrap();
+            let document = Html::parse_document(&data);
 
-        let house_name = document
-            .select(&house_name_selector)
-            .next()
-            .unwrap()
-            .first_child()
-            .unwrap()
-            .value();
-        println!("{:?}", house_name);
-        let calendars = document.select(&calendar_selector);
-        let mut check_outs: Vec<NaiveDate> = Vec::new();
-        let mut check_ins: Vec<NaiveDate> = Vec::new();
-
-        for calendar in calendars {
-            let month = calendar
-                .first_child()
-                .unwrap()
-                .next_sibling()
+            let house_name = document
+                .select(&house_name_selector)
+                .next()
                 .unwrap()
                 .first_child()
                 .unwrap()
                 .value();
-            let fragment = Html::parse_fragment(&calendar.html());
-            let checkout_selector = Selector::parse("div.calendar-checkout").unwrap();
-            let dates = fragment.select(&checkout_selector);
-            for date in dates {
-                let check_out_date_url = date
-                    .prev_sibling()
-                    .unwrap()
-                    .value()
-                    .as_element()
-                    .and_then(|a| a.attr("href"));
-                if let Some(check_out_date) =
-                    date_regex.find(check_out_date_url.unwrap_or_default())
-                {
-                    check_outs.push(
-                        NaiveDate::parse_from_str(check_out_date.as_str(), "%Y-%m-%d").unwrap(),
-                    );
-                } else {
-                }
-            }
-            let fragment = Html::parse_fragment(&calendar.html());
-            let checkout_selector = Selector::parse("div.calendar-checkin").unwrap();
-            let dates = fragment.select(&checkout_selector);
-            for date in dates {
-                if let Some(check_out_div) = date.next_sibling() {
-                    if let Some(check_out_date) = check_out_div.first_child() {
-                        let mut day = check_out_date.value().as_text().unwrap().text.to_string();
-                        if day.len() < 2 {
-                            day = format!("0{day}")
-                        };
+            let calendars = document.select(&calendar_selector);
+            let mut check_outs: Vec<NaiveDate> = Vec::new();
+            let mut check_ins: Vec<NaiveDate> = Vec::new();
 
-                        let date_string = format!("{} {}", day, month.as_text().unwrap().text);
-                        check_ins.push(
-                            NaiveDate::parse_from_str(&date_string, "%d %B %Y").unwrap_or_default(),
-                        )
+            for calendar in calendars {
+                let month = calendar
+                    .first_child()
+                    .unwrap()
+                    .next_sibling()
+                    .unwrap()
+                    .first_child()
+                    .unwrap()
+                    .value();
+                let fragment = Html::parse_fragment(&calendar.html());
+                let checkout_selector = Selector::parse("div.calendar-checkout").unwrap();
+                let dates = fragment.select(&checkout_selector);
+                for date in dates {
+                    let check_out_date_url = date
+                        .prev_sibling()
+                        .unwrap()
+                        .value()
+                        .as_element()
+                        .and_then(|a| a.attr("href"));
+                    if let Some(check_out_date) =
+                        date_regex.find(check_out_date_url.unwrap_or_default())
+                    {
+                        check_outs.push(
+                            NaiveDate::parse_from_str(check_out_date.as_str(), "%Y-%m-%d").unwrap(),
+                        );
+                    } else {
+                    }
+                }
+                let fragment = Html::parse_fragment(&calendar.html());
+                let checkout_selector = Selector::parse("div.calendar-checkin").unwrap();
+                let dates = fragment.select(&checkout_selector);
+                for date in dates {
+                    if let Some(check_out_div) = date.next_sibling() {
+                        if let Some(check_out_date) = check_out_div.first_child() {
+                            let mut day =
+                                check_out_date.value().as_text().unwrap().text.to_string();
+                            if day.len() < 2 {
+                                day = format!("0{day}")
+                            };
+
+                            let date_string = format!("{} {}", day, month.as_text().unwrap().text);
+                            check_ins.push(
+                                NaiveDate::parse_from_str(&date_string, "%d %B %Y")
+                                    .unwrap_or_default(),
+                            )
+                        }
                     }
                 }
             }
+            HouseDates {
+                house_name: house_name.as_text().unwrap().text.to_string(),
+                check_ins: check_ins.clone(),
+                check_outs: check_outs.clone(),
+            }
         }
-        let house = HouseDates {
-            house_name: house_name.as_text().unwrap().text.to_string(),
-            check_ins: check_ins.clone(),
-            check_outs: check_outs.clone(),
-        };
-        println!("{:?}", house);
-    });
+    }))
+    .await;
 
     for task in tasks {
-        task.await;
+        println!("{:?}", task)
     }
 }
